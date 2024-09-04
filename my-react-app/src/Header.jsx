a9d611bb399ee3e488d94d6d465ad9713441be30
Barbara <subwaynaas@yahoo.ie>
2024-09-04 07:18:32 +0300

    favicon

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -1,11 +1,12 @@
+import Img from "./assets/tomandbarbara.png";
+
 export default function Header() {
   return (
     <header className="header">
       <h1>Golf Majors Quiz created especially for</h1>
       <p className="name">Tom Ward</p>
       <small>
-        made with love from Barbara{" "}
-        <img src="./dist/tomandbarbara.JPG" alt="" />
+        made with love from Barbara <img src={Img} alt="" />
       </small>
     </header>
   );
